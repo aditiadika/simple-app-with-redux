ce31c5c36d0f932ec59fe1b9b519a4fd3c36bbf9
aditiadika <aditiaaadicka@gmail.com>
2018-08-28 15:47:23 +0700

    finish, add animation and action

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CardSection } from './common';
-import { Text, StyleSheet, TouchableWithoutFeedback, View } from 'react-native';
+import { Text, StyleSheet, TouchableWithoutFeedback, View, LayoutAnimation, Platform, UIManager } from 'react-native';
 import * as actions from '../actions';
+if (Platform.OS === 'android') { UIManager.setLayoutAnimationEnabledExperimental(true) }
 
 class ListItem extends Component {
+
+    componentWillUpdate() {
+        LayoutAnimation.spring();
+    }
+
+    renderDiscription() {
+        const { library, expanded } = this.props;
+
+        if (expanded) {
+            return (
+                <CardSection>
+                    <Text style={{ flex:1 }}>
+                        {library.description}
+                    </Text>
+                </CardSection>
+            )
+        }
+    }
+
     render() {
         const { id, title } = this.props.library;
 
@@ -14,6 +34,7 @@ class ListItem extends Component {
                     <CardSection>
                         <Text style={styles.headerText}>{title}</Text>
                     </CardSection>
+                    {this.renderDiscription()}
                 </View>
             </TouchableWithoutFeedback>
         );
@@ -24,7 +45,14 @@ const styles = StyleSheet.create({
     headerText: {
         fontSize: 18,
         paddingLeft: 15,
+        textAlign: 'center',
     }
 })
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = (state, ownProps) => {
+  const expanded = state.selectedLibraryId === ownProps.library.id;
+
+  return { expanded }
+}
+
+export default connect(mapStateToProps, actions)(ListItem);
--- src/reducers/SelectionReducers.js
+++ src/reducers/SelectionReducers.js
@@ -1,3 +1,10 @@
-export default () => {
-    return null;
+export default (state = null, action) => {
+    switch(action.type) {
+        case 'select_library':
+            return action.payload;
+        default:
+            return state;
+    }
 }
+
+// jika user di action index pilih  select library maka -> return action.payload;
